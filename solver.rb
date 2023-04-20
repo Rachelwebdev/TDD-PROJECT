964c93c121dfa9a0bb1248d935a91897cd36ab59
Joseph David <jodavid444dave@gmail.com>
2023-04-20 15:08:04 +0100

    Modify file structure and fix linters error

--- solver.rb
+++ solver.rb
@@ -1,25 +1,24 @@
 class Solver
+  def fizzbuzz(number)
+    if (number % 15).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+
   def factorial(number)
-    raise Expection, 'Negative numbers are not allowed' if number.negative?
-    return 1 if number == 1
+    return raise StandardError, 'Cannot find a factorial for a negative number' if number.negative?
     return 1 if number.zero?
 
     number * factorial(number - 1)
   end
 
-  def reverse(word)
-    word.chars.reverse.join
-  end
-
-  def fizzbuzz(number)
-    if (number % 5).zero? && (number % 3).zero?
-      'Fizzbuzz'
-    elsif (number % 5).zero?
-      'Buzz'
-    elsif (number % 3).zero?
-      'Fizz'
-    else
-      number.to_s
-    end
+  def reverse(str)
+    str.reverse
   end
 end
