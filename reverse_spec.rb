cde4eb2c05f47384dca4af1bdb41c5ecc90dbf6f
Joseph David <jodavid444dave@gmail.com>
2023-04-20 13:57:30 +0100

    Add fizzbuzz method

--- reverse_spec.rb
+++ reverse_spec.rb
@@ -4,13 +4,13 @@ describe Solver do
   context 'Reverse Function Unit Testing'
   it 'Should return hsab' do
     solver = Solver.new
-    result = solver.reverse('Bash')
-    expect(result).to eq('hsaB')
+    result = solver.reverse('Rachel')
+    expect(result).to eq('lehcaR')
   end
 
   it 'Should return dauf' do
     solver = Solver.new
-    result = solver.reverse('Fuad')
-    expect(result).to eq('dauF')
+    result = solver.reverse('Joe')
+    expect(result).to eq('eoJ')
   end
 end
--- solver.rb
+++ solver.rb
@@ -10,5 +10,16 @@ class Solver
   def reverse(word)
     word.chars.reverse.join
   end
-  
+
+  def fizzbuzz(number)
+    if (number % 5).zero? && (number % 3).zero?
+      'Fizzbuzz'
+    elsif (number % 5).zero?
+      'Buzz'
+    elsif (number % 3).zero?
+      'Fizz'
+    else
+      number.to_s
+    end
+  end
 end
