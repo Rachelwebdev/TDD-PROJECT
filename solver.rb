b6556b02ca901b4ceb9ce64e90370f648a55ecfa
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-20 14:55:11 +0400

    Add factorial method

---
+++ solver.rb
@@ -0,0 +1,10 @@
+class Solver
+    def factorial(number)
+    raise Expection, 'Negative numbers are not allowed' if number.negative?
+    return 1 if number == 1
+    return 1 if number.zero?
+
+    number * factorial(number - 1)
+  end
+
+end
